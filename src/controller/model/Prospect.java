d48022dbf71a98d6edf34a529b38a28cc0d37711
imohammadian <imoh.bilal@yahoo.fr>
2017-07-27 14:17:32 +0200

    ajout Prospect and date

--- src/controller/model/Prospect.java
+++ src/controller/model/Prospect.java
@@ -1,5 +1,45 @@
 package controller.model;
 
-public class Prospect {
+import java.util.Date;
 
-}
+import application.Adresse;
+
+public class Prospect extends Personnes {
+	
+		
+		private Date date ;
+		protected int NbCommande;
+
+		
+		public Prospect(String nom, String prenom, String rue, String ville, String pays, int codePostal) {
+			
+			super(nom, prenom, rue, ville, pays, codePostal);
+			
+			date = "";
+			NbCommande = 0;
+		}
+
+		
+		
+		public Date getDate() {
+			return date;
+		}
+
+		public void setDate(Date date) {
+			this.date = date;
+		}
+
+		public int getNbCommande() {
+			return NbCommande;
+		}
+
+		public void setNbCommande(int nbCommande) {
+			NbCommande = nbCommande;
+		}
+
+	
+		
+		
+		
+		
+	}
